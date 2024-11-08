a34f9e69e3328865176ce0fdf72282c5ea04c9c5
effortlessdevsec <effortlessdevsec@Mac.lan>
2024-11-08 12:41:29 +0530

    Initial commit

---
+++ static/img/docusaurus.config.js
@@ -0,0 +1,123 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Docusaurus Portfolio',
+  tagline: 'Github style portfolios for websites built on Docusaurus.',
+  url: 'https://github.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'silva-nick', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-portfolio', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Home',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        { to: '/me', label: 'About Me', position: 'left' },
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Documentation',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/silva-nick/docusaurus-portfolio',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    // // footer: {
+    // //   style: 'dark',
+    // //   links: [
+    // //     {
+    // //       title: 'Info',
+    // //       items: [
+    // //         {
+    // //           label: 'About me',
+    // //           to: '/docs/intro',
+    // //         },
+    // //       ],
+    // //     },
+    // //     {
+    // //       title: 'Docs',
+    // //       items: [
+    // //         {
+    // //           label: 'Documentation',
+    // //           to: '/docs/intro',
+    // //         },
+    // //       ],
+    // //     },
+    // //     {
+    // //       title: 'More',
+    // //       items: [
+    // //         {
+    // //           label: 'Blog',
+    // //           to: '/blog',
+    // //         },
+    // //         {
+    // //           label: 'GitHub',
+    // //           href: 'https://github.com/silva-nick/docusaurus-portfolio',
+    // //         },
+    // //       ],
+    // //     },
+    // //   ],
+    // //   copyright: `Copyright © ${new Date().getFullYear()} docusaurus-portfolio, Inc. Built with Docusaurus.`,
+    // // },
+    // prism: {
+    //   theme: lightCodeTheme,
+    //   darkTheme: darkCodeTheme,
+    // },
+  },
+  themes: ['docusaurus-portfolio-theme'],
+  plugins: [
+    [
+      'docusaurus-portfolio-plugin',
+      {
+        username: 'silva-nick',
+        path: '/me',
+        pageTitle: "Nick's site",
+        pageDescription: 'About me, nick.',
+        userOptions: {
+          fullname: 'Nick Silva',
+        },
+        repoOptions: {
+          type: 'all',
+          sort: 'updated',
+          direction: 'desc',
+          numRepos: 100,
+        },
+      },
+    ],
+  ],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
