2d951e88f29cf0b3abeb46c7ce30cbee15c94034
effortlessdevsec <effortlessdevsec@effortlessdevsecs-MacBook-Pro.local>
2026-03-14 22:31:11 +0530

    Deploy website - based on 4ef9be8fc8f8d531c19cf81ab1bce1321b0bc64d

--- assets/js/acecf23e.86a288c1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkeffortlessdevsec=globalThis.webpackChunkeffortlessdevsec||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/effortlesssecurity/blog","blogTitle":"Blog","authorsListPath":"/effortlesssecurity/blog/authors"}')}}]);
